6486eba31e6771ef5a35fcd750f1235a5e6ac213
Guest-11111111 <nanda.swagatika@gmail.com>
2026-01-27 17:57:27 -0600

    add pyscript for python support add electron app details learn basic syntax

---
+++ Physium/index.js
@@ -0,0 +1,19 @@
+const { app, BrowserWindow } = require('electron');
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true // Standard practice for accessing Node APIs
+    }
+  });
+  win.loadFile('index.html');
+}
+
+app.whenReady().then(createWindow);
+
+// Quit when all windows are closed, except on macOS
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
